325309aa27f471458e46e4a60037c6695e02facb
Leandro Franco <llefranco@gmail.com>
2026-02-04 22:07:08 -0300

    v0.1

---
+++ desafio_personagem.py
@@ -0,0 +1,74 @@
+'''🧠 Atividade Prática Classificador de Personagens + Escolha de Ação | Dia 04/02
+
+📋 Descrição da Tarefa:
+
+
+Você está criando um pequeno sistema de um jogo de aventura onde o jogador será classificado por sua experiência e, com base em sua escolha, executará uma ação dentro do jogo.
+
+ 
+🔧 O que seu programa deve fazer:
+
+ 
+1.Pedir ao jogador quantos pontos de experiência ele tem (XP):
+
+Menos de 100 → "Iniciante"
+
+Entre 100 e 500 → "Intermediário"
+
+Mais de 500 → "Veterano"
+
+Use if/elif/else para essa classificação.
+
+
+2. Depois, o programa deve perguntar qual ação o jogador deseja executar (usar match case):
+
+"A" → Atacar
+
+"D" → Defender
+
+"F" → Fugir
+
+Qualquer outra tecla → "Ação inválida"
+
+
+Mostre uma mensagem apropriada para cada ação, como:
+
+"Você avançou para o ataque!"
+
+"Você levantou o escudo!"
+
+"Você fugiu da batalha!"
+
+ 📝 Regras de Entrega:
+
+Crie seu código em um arquivo .py
+Faça testes com diferentes níveis de XP e ações
+Envie o código por GitHub ou por sua plataforma de aulas
+
+'''
+print("Bem vindo ao Coliseu, vamos nos preparar para a luta!")
+
+experiencia = int(input("Quantos pontos de experiência você possui, aventureiro?: "))
+
+
+if experiencia < 100: 
+    print ("Aventureiro Iniciante")
+elif  100 >= experiencia < 500:
+    print ("Aventureiro Intermediário")
+else:
+    print ("Aventureiro Veterano")
+
+print("Certo, lembre-se que na luta você pode Atacar (A), Defender (D) ou Fugir (F), boa sorte!")
+
+acao = input("Começou a luta! Qual será sua ação?: ")
+
+match acao:
+    case "A":
+        print ("Você ataca o inimigo!")
+    case "D":
+        print ("Você levanta seu escudo e entra em uma posição defensiva!")
+    case "F":
+        print ("Você tenta fugir, mas tropeça e cai no chão!")
+    case _:
+        print ("Esse comando não existe, use (A) para atacar, (D) para defender ou (F) para fugir.")
+
